fc4bc83a5b658f867f723e3b5967cf9abf54b318
matheesha-20 <matheeshakavindu32@gmail.com>
2025-06-01 17:00:12 +0200

    DBConnection

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -1,4 +1,23 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static DBConnection instance;
+
+    private Connection connection;
+    private DBConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/managment_system", "root", "1234");
+    }
+    public  Connection getConnection(){
+        return connection;
+    }
+    public static DBConnection getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DBConnection();
+        }
+        return instance;
+    }
 }
